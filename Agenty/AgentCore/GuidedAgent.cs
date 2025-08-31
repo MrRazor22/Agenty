5a7ca66b0728a3cf4a21727f3a280bda7fb5335d
MrRazor22 <senthilvenkat22@gmail.com>
2025-09-01 03:08:11 +0530

    GUIDED AGENT ADDED

---
+++ Agenty/AgentCore/GuidedAgent.cs
@@ -0,0 +1,96 @@
+﻿using Agenty.LLMCore;
+using Agenty.LLMCore.Providers.OpenAI;
+using Agenty.LLMCore.ToolHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Agenty.LLMCore;
+using Agenty.LLMCore.Logging;
+using Agenty.LLMCore.Providers.OpenAI;
+using Agenty.LLMCore.ToolHandling;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json.Nodes;
+using ILogger = Agenty.LLMCore.Logging.ILogger;
+
+namespace Agenty.AgentCore
+{
+    public sealed class GuidedAgent : IAgent
+    {
+        private ILLMClient _llm = null!;
+        private ToolCoordinator _coord = null!;
+        private readonly IToolRegistry _tools = new ToolRegistry();
+        Conversation _globalChat;
+        Grader? _grader;
+        ILogger _logger;
+        string _systemPrompt = "You are an assistant. " +
+                "For complex tasks, always plan step by step. " +
+                "If unsure, say so directly. " +
+                "Use tools if needed, or respond directly. " +
+                "Keep answers short and clear.";
+
+        public static GuidedAgent Create() => new();
+        private GuidedAgent() { }
+
+        public GuidedAgent WithLLM(string baseUrl, string apiKey, string model = "any_model")
+        {
+            _llm = new OpenAILLMClient();
+            _llm.Initialize(baseUrl, apiKey, model);
+            _coord = new ToolCoordinator(_llm, _tools);
+            _globalChat = new Conversation().Add(Role.System, _systemPrompt);
+            return this;
+        }
+
+        public GuidedAgent WithTools<T>() { _tools.RegisterAll<T>(); return this; }
+        public GuidedAgent WithTools(params Delegate[] fns) { _tools.Register(fns); return this; }
+        public GuidedAgent WithLogger(ILogger logger)
+        {
+            _logger = logger;
+            _grader = new Grader(_coord, logger);
+            return this;
+        }
+
+        public async Task<string> ExecuteAsync(string goal, int maxRounds = 10)
+        {
+            var sessionChat = new Conversation();
+            var sum = new SummaryResult("");
+
+            _logger.AttachTo(sessionChat);
+
+            sessionChat.Add(Role.System, _systemPrompt)
+                .Add(Role.User, goal);
+
+            for (int round = 0; round < maxRounds; round++)
+            {
+                var response = await _coord.GetToolCalls(sessionChat);
+
+                await ExecuteToolChaining(response, sessionChat);
+
+                sum = await _grader!.SummarizeConversation(sessionChat, goal);
+                var answer = await _grader!.CheckAnswer(goal, sum.summary);
+
+                if (answer.confidence_score >= 80)
+                {
+                    _globalChat.Add(Role.User, goal)
+                                .Add(Role.Assistant, sum.summary);
+                    return sum.summary;
+                }
+            }
+            return await _llm.GetResponse(
+                sessionChat.Add(Role.User,
+                $"Answer the user’s request: {goal}. Use the available tool results and reasonings so far. Make the response clear and user-friendly."));
+        }
+
+        private async Task ExecuteToolChaining(LLMResponse response, Conversation chat)
+        {
+            while (response.ToolCalls.Count != 0)
+            {
+                await _coord.HandleToolCallSequential(response.ToolCalls, chat);
+
+                response = await _coord.GetToolCalls(chat);
+            }
+        }
+
+    }
+}
