98ea94f9e6255625be1331aba5c1ea648c1572e4
MrRazor22 <senthilvenkat22@gmail.com>
2025-11-23 00:11:53 +0530

    Add Agenty.Utils namespace to LLMClientBase.cs
    Introduced the `Agenty.Utils` namespace to the `LLMClientBase.cs` file.
    This change enables the use of utilities or functionality provided
    by the `Agenty.Utils` library. No existing code was modified or
    removed as part of this update.

--- Agenty/LLMCore/Runtime/LLMClientBase.cs
+++ Agenty/LLMCore/Runtime/LLMClientBase.cs
@@ -2,6 +2,7 @@
 using Agenty.LLMCore.JsonSchema;
 using Agenty.LLMCore.TokenHandling;
 using Agenty.LLMCore.ToolHandling;
+using Agenty.Utils;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
